51ef1f6625ab5a8604e08d3c8f363d772ba9c9ba
Rohan Palankar <rohanpalankar8@gmail.com>
2025-12-25 14:45:06 +0530

    initial commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://mvdjekfduqswokzjagzi.supabase.co";
+export const supabaseUrl = "https://mvdjekfduqswokzjagzi.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12ZGpla2ZkdXFzd29remphZ3ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1OTIyMjYsImV4cCI6MjA3ODE2ODIyNn0.2FnAFzf6nS5I00cbMcsXmby8Q9yjJUErBHUjgKluvSY";
 const supabase = createClient(supabaseUrl, supabaseKey);
